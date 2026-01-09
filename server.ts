8fcb2ef195d4e3f52b2811274b8edd7a6e8c0ee7
YSO-DEV <y.soulinthavong@gmail.com>
2026-01-09 16:56:17 +0100

    Add: simple gateway

--- server.ts
+++ server.ts
@@ -1,34 +1,52 @@
-import express from "express";
-import dotenv from "dotenv";
-import bestiaire from "./MicroServiceBestiaire/src/services/Creature";
-import Temoignage from "./MicroServiceBestiaire/src/services/Temoignage";
-import auth from "./MicroServiceAuth/src/services/Auth";
-import { connectDatabase as connectAuthDatabase } from "./MicroServiceAuth/src/data/connectDatabase";
-import { connectDatabase as connectBestiaireDatabase } from "./MicroServiceBestiaire/src/data/connectDatabase";
+// import express from "express";
+// import dotenv from "dotenv";
+// import bestiaire from "./MicroServiceBestiaire/src/services/Creature";
+// import Temoignage from "./MicroServiceBestiaire/src/services/Temoignage";
+// import auth from "./MicroServiceAuth/src/services/Auth";
+// import { connectDatabase as connectAuthDatabase } from "./MicroServiceAuth/src/data/connectDatabase";
+// import { connectDatabase as connectBestiaireDatabase } from "./MicroServiceBestiaire/src/data/connectDatabase";
 
-dotenv.config({
-    quiet: true,
+// dotenv.config({
+//     quiet: true,
+// });
+
+// const SERVER_PORT = process.env.SERVER_PORT;
+
+// const app = express();
+
+// app.use(express.json());
+// app.use("/", bestiaire);
+// app.use("/", Temoignage);
+// app.use("/", auth);
+
+// (async () => {
+//     try {
+//         await connectAuthDatabase();
+//         await connectBestiaireDatabase();
+
+//         app.listen(SERVER_PORT, () => {
+//             console.log(`Listen to Port: http://localhost:${SERVER_PORT}`);
+//         });
+//     } catch (err) {
+//         console.error("Failed to connect to database", err);
+//         process.exit(1);
+//     }
+// })();
+
+
+
+
+import { Router } from "express";
+import axios from "axios";
+
+const router = Router();
+
+router.post("/login", async (req, res) => {
+  const response = await axios.post(
+    "http://localhost:3001/auth/login",
+    req.body
+  );
+  res.json(response.data);
 });
 
-const SERVER_PORT = process.env.SERVER_PORT;
-
-const app = express();
-
-app.use(express.json());
-app.use("/", bestiaire);
-app.use("/", Temoignage);
-app.use("/", auth);
-
-(async () => {
-    try {
-        await connectAuthDatabase();
-        await connectBestiaireDatabase();
-
-        app.listen(SERVER_PORT, () => {
-            console.log(`Listen to Port: http://localhost:${SERVER_PORT}`);
-        });
-    } catch (err) {
-        console.error("Failed to connect to database", err);
-        process.exit(1);
-    }
-})();
+export default router;
